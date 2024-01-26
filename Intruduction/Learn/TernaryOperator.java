c128a1f1be752ae5b74666e59674897b40f29880
AbdulKhadarZilanKothakota <153972670+AbdulKhadarZilanKothakota@users.noreply.github.com>
2024-01-27 03:14:37 +0530

    Add files via upload

---
+++ Intruduction/Learn/TernaryOperator.java
@@ -0,0 +1,10 @@
+// using ternary operator
+class Main
+{
+    public static void main(String[] args)
+    {
+        int i=10;
+        String s=(i%2==0)?"even":"odd";
+        System.out.println(s);
+    }
+}
