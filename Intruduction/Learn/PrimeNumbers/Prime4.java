cf4c50a5c27d28d72ce9d469db924d28a1bdba17
AbdulKhadarZilanKothakota <153972670+AbdulKhadarZilanKothakota@users.noreply.github.com>
2024-01-30 02:29:01 +0530

    Add files via upload

---
+++ Intruduction/Learn/PrimeNumbers/Prime4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main{
+
+    static String isPrime(int n){
+         
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return "Not Prime";
+            }
+            
+        }
+        return "Prime";
+        
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = scan.nextInt();
+
+        String result = isPrime(n);
+        System.out.println(result);
+    }
+}
