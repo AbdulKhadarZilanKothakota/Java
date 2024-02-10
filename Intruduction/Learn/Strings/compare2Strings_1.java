0fd87a0ae0a19e618daf3133c97173eecd7ae8d3
AbdulKhadarZilanKothakota <153972670+AbdulKhadarZilanKothakota@users.noreply.github.com>
2024-02-10 19:45:18 +0530

    Add files via upload

---
+++ Intruduction/Learn/Strings/compare2Strings_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+        String s1 = "ABC";
+        String s2 = "ABC";
+        if(s1==s2){
+            System.out.println("References are Equal"); // References are Equal
+        }
+        else{
+            System.out.println("References are Not Equal");
+        }
+    }
+}
