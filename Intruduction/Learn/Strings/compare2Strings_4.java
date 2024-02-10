9e489120b67ca744c0a1f41db32df9fe452c9a5c
AbdulKhadarZilanKothakota <153972670+AbdulKhadarZilanKothakota@users.noreply.github.com>
2024-02-10 19:48:18 +0530

    Add files via upload

---
+++ Intruduction/Learn/Strings/compare2Strings_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+        String s1 = "ABC";
+        String s2 = "abc";
+        // String s2 = new String("abc");
+        if(s1.equals(s2)){
+            System.out.println("Values are Equal");
+        }
+        else{
+            System.out.println("Values are Not Equal");   //Values are Not Equal
+        }
+    }
+}
